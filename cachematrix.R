93b3c30543a45ed0f8fd51e78e817423117a47d5
iHowley <ihowley@gmail.com>
2016-07-21 14:28:42 -0700

    Updating vars/functions for invert matrix
    Switching the purpose from a mean of a vector to inverting a matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,10 @@
 ## inversion that we will not discuss here). Your assignment is to write a pair of functions that 
 ## cache the inverse of a matrix.
 
+## In this example we introduce the <<- operator which can be used to assign a value to an object 
+## in an environment that is different from the current environment. Below are two functions that 
+## are used to create a special object that stores a numeric vector and cache's its mean.
+
 ## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -12,11 +16,11 @@ makeCacheMatrix <- function(x = matrix()) {
     m <<- NULL
   }
   get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
+  setInverse <- function(solve) m <<- solve
+  getInverse <- function() m
   list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
 
@@ -25,16 +29,19 @@ makeCacheMatrix <- function(x = matrix()) {
 ## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should 
 ## retrieve the inverse from the cache.
 
+## Computing the inverse of a square matrix can be done with the solve function in R. For example, if X is 
+## a square invertible matrix, then solve(X) returns its inverse.
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   
-  m <- x$getmean()
+  m <- x$getInverse()
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
   data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
+  m <- solve(data, ...)
+  x$setInverse(m)
   m
 }
